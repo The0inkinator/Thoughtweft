37f6affc0656d7dd070a3aa9112f65e3eb9a1243
Keldan Campbell <theoinkinator@gmail.com>
2024-03-20 15:57:23 -0700

    remove template files

--- src/routes/(Home).tsx
+++ src/routes/(Home).tsx
@@ -2,9 +2,6 @@ import { Title } from "@solidjs/meta";
 
 export default function Home() {
   return (
-    <main>
-      <Title>About</Title>
-      <h1>About</h1>
-    </main>
+<div>hello</div>
   );
 }

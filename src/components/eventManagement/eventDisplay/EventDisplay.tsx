87e69dac9496e1b3c9de873b9bed8765914db229
Keldan Campbell <theoinkinator@gmail.com>
2024-03-22 20:40:21 -0700

    move input to event display

--- src/components/eventManagement/eventDisplay/EventDisplay.tsx
+++ src/components/eventManagement/eventDisplay/EventDisplay.tsx
@@ -1,8 +1,42 @@
-import { For } from "solid-js";
+import "./eventDisplay.css";
+import { For, createSignal } from "solid-js";
 import { useEventContext } from "~/context/EventContext";
+import PodCard from "../podCard/PodCard";
 
 export default function EventDisplay() {
+  //Context State
   const [eventState] = useEventContext();
-  console.log(eventState());
-  return <div class="eventDisplayContainer"></div>;
+  //Local State
+  const [inputValue, setInputValue] = createSignal<string>("");
+
+  return (
+    <div class="eventDisplayContainer">
+      <div id="playerNameInputContainer">
+        <input
+          id="playerNameInput"
+          type="text"
+          placeholder="Player's Name"
+          value={inputValue()}
+          onInput={(event) => {
+            setInputValue(event.target.value);
+          }}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") {
+              // add player function
+              setInputValue("");
+            }
+          }}
+        ></input>
+        <button
+          id="playerNameSubmit"
+          type="submit"
+          onClick={() => {
+            // add player function
+            setInputValue("");
+          }}
+        ></button>
+      </div>
+      <For each={eventState()}>{(pod) => <PodCard />}</For>
+    </div>
+  );
 }
--- src/components/eventManagement/podCard/PodCard.tsx
+++ src/components/eventManagement/podCard/PodCard.tsx
@@ -1,48 +1,15 @@
 import "./podCard.css";
 import { createSignal, For } from "solid-js";
 
-//TYPING
-interface PodInputs {
-  id: number;
-}
-
 //MAIN FUNCTION
-export default function PodBox({ id }: PodInputs) {
+export default function PodCard() {
   //Context State
 
-  //State
-  const [inputValue, setInputValue] = createSignal<string>("");
-
   return (
-    <div class="podBoxContainer">
-      <div class="podNumber">Pod # {id}</div>
+    <div class="podCardContainer">
+      <div class="podNumber">Pod #</div>
       {/* replace empty array below */}
       <For each={[]}>{(playerObj) => <>{/* playerCard */}</>}</For>
-      <div id="playerNameInputContainer">
-        <input
-          id="playerNameInput"
-          type="text"
-          placeholder="Player's Name"
-          value={inputValue()}
-          onInput={(event) => {
-            setInputValue(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            if (event.key === "Enter") {
-              // add player function
-              setInputValue("");
-            }
-          }}
-        ></input>
-        <button
-          id="playerNameSubmit"
-          type="submit"
-          onClick={() => {
-            // add player function
-            setInputValue("");
-          }}
-        ></button>
-      </div>
     </div>
   );
 }

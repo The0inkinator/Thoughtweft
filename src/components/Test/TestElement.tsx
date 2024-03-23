9aaad3b81642d7252f35d16bf5d74b98447331bb
Keldan Campbell <theoinkinator@gmail.com>
2024-03-23 14:39:19 -0700

    update test element

--- src/components/Test/TestElement.tsx
+++ src/components/Test/TestElement.tsx
@@ -1,18 +1,26 @@
 import { createSignal } from "solid-js";
-import "./TestElement.css";
+import styles from "./TestElement.module.css";
 import { useTestContext } from "~/context/TestContext";
 
 type ElementStates = "inSlot" | "dragging";
 
 export default function TestElement() {
+  let ball!: HTMLDivElement;
+
   const [testState, { updateTestStateData, secondFunction }] = useTestContext();
   const [elementState, setElementState] = createSignal<ElementStates>("inSlot");
 
   return (
-    <div class="TestElement">
-      <div id="element"></div>
-      <div class="bucketOne"></div>
-      <div class="bucketTwo"></div>
+    <div class={styles.testElement}>
+      <div
+        id={styles.element}
+        ref={ball}
+        onClick={() => {
+          console.log(ball.getBoundingClientRect());
+        }}
+      ></div>
+      <div class={styles.bucketOne}></div>
+      <div class={styles.bucketTwo}></div>
     </div>
   );
 }

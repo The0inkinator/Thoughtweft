98f894d8874f4a7aac1c507e3cee4c4d81f790dc
Keldan Campbell <theoinkinator@gmail.com>
2024-06-21 21:45:08 -0700

    add draft timer

--- src/components/eventManagement/podCard/PodCard.tsx
+++ src/components/eventManagement/podCard/PodCard.tsx
@@ -266,6 +266,57 @@ export default function PodCard({ podSize, podNumber, podId }: PodCardInputs) {
     );
   };
 
+  //Timer logic
+  const [draftTimerHou, setDraftTimerHou] = createSignal<number>(0);
+  const [draftTimerMin, setDraftTimerMin] = createSignal<number>(0);
+  const [draftTimerSec, setDraftTimerSec] = createSignal<number>(0);
+  let draftTimerStarted = false;
+
+  const countTimerDown = () => {};
+
+  createEffect(() => {
+    if (
+      draftTimerStarted === false &&
+      thisPodState()?.podStatus === "drafting"
+    ) {
+      draftTimerStarted = true;
+      const totalTime = thisPodState()!.podDraftTime;
+      const totalHours = Math.floor(totalTime / 60);
+      const totalMins = totalTime - totalHours * 60;
+      const totalSecs = 0;
+      let tempSec;
+      let tempMin;
+      let tempHour;
+      setDraftTimerHou(totalHours);
+      setDraftTimerMin(totalMins);
+      setDraftTimerSec(totalSecs);
+
+      const loop = () => {
+        if (draftTimerSec() > 0) {
+          tempSec = draftTimerSec() - 1;
+          setDraftTimerSec(tempSec);
+          setTimeout(loop, 1000);
+        } else if (draftTimerMin() > 0) {
+          tempMin = draftTimerMin() - 1;
+          tempSec = 59;
+          setDraftTimerMin(tempMin);
+          setDraftTimerSec(tempSec);
+          setTimeout(loop, 1000);
+        } else if (draftTimerHou() > 0) {
+          tempHour = draftTimerHou() - 1;
+          tempMin = 59;
+          tempSec = 59;
+          setDraftTimerHou(tempHour);
+          setDraftTimerMin(tempMin);
+          setDraftTimerSec(tempSec);
+          setTimeout(loop, 1000);
+        }
+      };
+
+      setTimeout(loop, 1000);
+    }
+  });
+
   const DraftingPodCard = () => {
     return (
       <>
@@ -279,6 +330,9 @@ export default function PodCard({ podSize, podNumber, podId }: PodCardInputs) {
         >
           Advance to Round 1
         </button>
+        <div>
+          {draftTimerHou()} {draftTimerMin()} {draftTimerSec()}
+        </div>
       </>
     );
   };

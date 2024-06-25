b52c7aefb7bdbbcc87f61abca4bc68efd3932621
Keldan Campbell <theoinkinator@gmail.com>
2024-06-25 16:17:47 -0700

    add complete match button

--- src/components/eventManagement/matchCard/MatchCard.tsx
+++ src/components/eventManagement/matchCard/MatchCard.tsx
@@ -1,6 +1,7 @@
 import styles from "./matchCard.module.css";
 import PlayerSeat from "../playerSeat/PlayerSeat";
 import { Match } from "~/typing/eventTypes";
+import { useEventContext } from "~/context/EventContext";
 
 interface MatchCardInputs {
   podId: number;
@@ -8,9 +9,30 @@ interface MatchCardInputs {
 }
 
 export default function MatchCard({ podId, matchInfo }: MatchCardInputs) {
+  const [eventState, { updateMatch }] = useEventContext();
+
+  const thisMatchState = () => {
+    return eventState()
+      .evtPods.find((pod) => pod.podId === podId)
+      ?.podMatches.find((match) => match.matchId === matchInfo.matchId);
+  };
+
   return (
     <>
       <div class={styles.matchCont}>
+        <button
+          type="submit"
+          style={{
+            "background-color": thisMatchState()?.matchCompleted
+              ? "green"
+              : "red",
+          }}
+          onclick={() => {
+            updateMatch(podId, matchInfo.matchId, { matchCompleted: true });
+          }}
+        >
+          Complete Match
+        </button>
         <PlayerSeat
           seatNumber={matchInfo.player1Seat}
           podId={podId}

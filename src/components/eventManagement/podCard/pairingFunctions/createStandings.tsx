ca67eb2fba51d24067c80a2ae7fd082b4a0cb047
Keldan Campbell <theoinkinator@gmail.com>
2024-07-02 10:07:56 -0700

    update standings to reflect players

--- src/components/eventManagement/podCard/pairingFunctions/createStandings.tsx
+++ src/components/eventManagement/podCard/pairingFunctions/createStandings.tsx
@@ -18,18 +18,37 @@ type PlayerStanding = {
 type Standings = PlayerStanding[];
 
 export default function CreateStandings(eventData: Event, podId: number) {
-  const finalStandings: Standings = [];
+  const pod = eventData.evtPods.find((pod) => pod.podId === podId)!;
+  const podPlayers = eventData.evtPlayerList.filter(
+    (player) => player.podId === podId
+  )!;
+  const matches = pod.podMatches;
 
-  const sampleEntry: PlayerStanding = {
-    rank: 1,
-    name: "Sally",
-    points: 12,
-    record: { w: 4, l: 1, d: 0 },
-    omw: 90,
-    gw: 95,
-    ogw: 65,
-  };
+  const tempStandings: Standings = [];
 
-  finalStandings.push(sampleEntry);
+  podPlayers.map((player) => {
+    let pointTotal: number = 0;
+    let finalRecord: PlayerStanding["record"] = { w: 0, l: 0, d: 0 };
+    let omwCalc: number = 0;
+    let gwCalc: number = 0;
+    let ogwCalc: number = 0;
+
+    matches.filter((match) => match);
+
+    const standingEntry: PlayerStanding = {
+      rank: 0,
+      name: player.name,
+      points: pointTotal,
+      record: finalRecord,
+      omw: omwCalc,
+      gw: gwCalc,
+      ogw: ogwCalc,
+    };
+
+    tempStandings.push(standingEntry);
+  });
+
+  const finalStandings = tempStandings.sort((a, b) => b.rank - a.rank);
+
   return finalStandings;
 }

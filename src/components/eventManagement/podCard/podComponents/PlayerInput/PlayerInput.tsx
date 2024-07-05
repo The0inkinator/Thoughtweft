96323624ac0f981b2e027d77e7ebaae61c22e30c
Keldan Campbell <theoinkinator@gmail.com>
2024-07-04 22:20:30 -0700

    add PlayerInput

---
+++ src/components/eventManagement/podCard/podComponents/PlayerInput/PlayerInput.tsx
@@ -0,0 +1,11 @@
+import { Event } from "~/typing/eventTypes";
+import styles from "./playerInput.module.css";
+
+interface PlayerInputInputs {
+  eventData: Event;
+  podId: number;
+}
+
+export default function PlayerInput({ eventData, podId }: PlayerInputInputs) {
+  return <></>;
+}
